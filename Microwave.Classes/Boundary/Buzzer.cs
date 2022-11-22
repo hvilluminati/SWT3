6980fc8d800f06d5943eda36e502eb3dc26553f4
Nadixo <75481929+Nadixo@users.noreply.github.com>
2022-11-22 14:20:39 +0100

    Made IBuzzer, Buzzer, and implemented it in UserInterface aswell as Program.cs

---
+++ Microwave.Classes/Boundary/Buzzer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Microwave.Classes.Interfaces;
+
+namespace Microwave.Classes.Boundary
+{
+    public class Buzzer : IBuzzer
+    {
+        private IOutput myOutput;
+        public Buzzer(IOutput output)
+        {
+            myOutput = output;
+        }
+        public void MakeBuzzerSound()
+        {
+            myOutput.OutputLine("Making Buzzer Sound");
+            Console.Beep(659, 205);
+            Thread.Sleep(125);
+            Console.Beep(659, 205);
+            Thread.Sleep(125);
+            Console.Beep(659, 205);
+            
+        }
+    }
+}
