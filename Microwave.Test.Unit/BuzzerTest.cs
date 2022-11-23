16250f060c0bf879de5cf645b4ae458fe5d20768
Nadixo <75481929+Nadixo@users.noreply.github.com>
2022-11-23 11:36:59 +0100

    Made Test for Buzzer class

---
+++ Microwave.Test.Unit/BuzzerTest.cs
@@ -0,0 +1,31 @@
+﻿using Microwave.Classes.Boundary;
+using Microwave.Classes.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+
+namespace Microwave.Test.Unit
+{
+    [TestFixture]
+    public class BuzzerTest
+    {
+        private Buzzer uut;
+        private IOutput output;
+
+        [SetUp]
+        public void Setup()
+        {
+            output = Substitute.For<IOutput>();
+
+            uut = new Buzzer(output);
+        }
+        [Test]
+        public void TurnOn_Buzzer()
+        {
+            uut.MakeBuzzerSound();
+            output.Received(1).OutputLine(Arg.Is<string>(str => str.Contains("Making Buzzer Sound")));
+        }
+
+
+    }
+}
